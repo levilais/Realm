b1ecef00246b79deda40d157a8d00f011fae7e7a
Levi Lais <levilais@gmail.com>
2018-09-06 21:24:43 -0700

    ARKit connected and looking good

---
+++ Realm/Assets/Textfield.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.iOS;
+
+public class Textfield : MonoBehaviour {
+
+
+    public void LaunchKeyboard() {
+        LaunchKeyboard();
+        Debug.Log("attempting to launch keyboard");
+    }
+
+	//// Use this for initialization
+	//void Start () {
+        
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
