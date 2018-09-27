869b1c17c764fa8bf9b1cd0b53e939f54ec74557
Levi Lais <levilais@gmail.com>
2018-09-27 11:07:14 -0700

    Added Display Info - next will be to populate correct inputs

---
+++ Realm/Assets/DisplayInfoView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayInfoView : MonoBehaviour {
+
+    private void OnEnable()
+    {
+        PopulateHeader();
+    }
+
+    private void PopulateHeader()
+    {
+        // this is where we'll set the name of the detail display to the name if this item
+        //We'll need to set the activeObject in the DisplaysView when the button is pressed. That's how we'll determine the title, too.
+        ViewController viewController = transform.GetComponent<ViewController>();
+        viewController.headerIcon.GetComponent<Image>().sprite = Resources.Load<Sprite>("Icons/DisplaysIcon");
+        viewController.headerTitle.GetComponent<Text>().text = "Display Info";
+    }
+
+}
